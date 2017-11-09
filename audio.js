fe840aca67373e9e37062a5e609d3b477b49d60d
vvw <siluplsy@gmail.com>
2017-11-09 15:08:40 +0800

    ''

---
+++ audio.js
@@ -0,0 +1,6 @@
+
+var log = console.log
+export function x() {
+	log('xxx...')
+}
+
---
+++ i.js
@@ -0,0 +1,11 @@
+
+var log = console.log
+import {x} from './audio.js'
+
+function sayhi() {
+	log('hi,,,')
+	x()
+}
+sayhi()
+// export default sayhi
+
